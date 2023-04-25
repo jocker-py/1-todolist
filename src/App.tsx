3c704d151fdb2bc322b11e4e532535636d9ec427
jocker-py <buladzislau@gmail.com>
2023-04-25 13:00:30 +0300

    refactor: rename titles

--- src/App.tsx
+++ src/App.tsx
@@ -20,11 +20,11 @@ function App() {
       {id: v1(), title: "GraphQL", isDone: false},
     ],
     [todoListId_2]: [
-      {id: v1(), title: "HTML&CSS", isDone: true},
-      {id: v1(), title: "JS", isDone: true},
-      {id: v1(), title: "ReactJS", isDone: false},
-      {id: v1(), title: "Rest API", isDone: false},
-      {id: v1(), title: "GraphQL", isDone: false},
+      {id: v1(), title: "Milk", isDone: true},
+      {id: v1(), title: "Sugar", isDone: true},
+      {id: v1(), title: "Tea", isDone: false},
+      {id: v1(), title: "Salt", isDone: false},
+      {id: v1(), title: "Coffee", isDone: false},
     ],
   });
 
