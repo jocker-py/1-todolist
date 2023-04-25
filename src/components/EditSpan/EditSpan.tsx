f7297afce6fa23cbd638794c0ab04ca1a0581d5f
jocker-py <buladzislau@gmail.com>
2023-04-25 15:06:30 +0300

    refactor: refactor code

--- src/components/EditSpan/EditSpan.tsx
+++ src/components/EditSpan/EditSpan.tsx
@@ -2,25 +2,27 @@ import React, {ChangeEvent, FC, useState} from "react";
 
 type EditSpanPropsType = {
   title: string
-  updateItem: (text: string) => void
+  onChange: (title: string) => void
 }
 
-const EditSpan: FC<EditSpanPropsType> = ({title, updateItem}) => {
-  const [text, setText] = useState(title);
+const EditSpan: FC<EditSpanPropsType> = ({title, onChange}) => {
+  const [text, setText] = useState("");
   const [editMode, setEditMode] = useState(false);
   const activeEditMode = () => {
+    setText(title);
     setEditMode(true);
   };
-  const disactiveEditMode = () => {
+  const activeViewMode = () => {
     setEditMode(false);
-    updateItem(text);
+    onChange(text);
   };
   const changeText = (e: ChangeEvent<HTMLInputElement>) => {
     setText(e.target.value);
   };
-  return (editMode ?
-    <input value={text} onChange={changeText} onBlur={disactiveEditMode} autoFocus/> :
-    <span onDoubleClick={activeEditMode}>{title}</span>);
+  return (
+    editMode ?
+      <input value={text} onChange={changeText} onBlur={activeViewMode} autoFocus/> :
+      <span onDoubleClick={activeEditMode}>{title}</span>);
 };
 
 export default EditSpan;
