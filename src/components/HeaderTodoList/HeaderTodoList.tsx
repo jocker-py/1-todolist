090b18f5d2363d3cb6cf42f99a77ef311fc6df6f
jocker-py <buladzislau@gmail.com>
2023-05-02 12:14:08 +0300

    refactor: use Button from MaterialUi instead of button

--- src/components/HeaderTodoList/HeaderTodoList.tsx
+++ src/components/HeaderTodoList/HeaderTodoList.tsx
@@ -1,6 +1,8 @@
 import React, {FC} from "react";
 import EditSpan from "../EditSpan/EditSpan";
 import {Variant} from "@mui/material/styles/createTypography";
+import {IconButton, Tooltip} from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 type HeaderTodoListPropsType = {
   title: string
@@ -10,11 +12,15 @@ type HeaderTodoListPropsType = {
   onChange: (title: string) => void
 }
 
-const HeaderTodoList: FC<HeaderTodoListPropsType> = ({title, variant, weight, onClick, onChange, }) => {
+const HeaderTodoList: FC<HeaderTodoListPropsType> = ({title, variant, weight, onClick, onChange}) => {
   return (
     <h3>
       <EditSpan title={title} onChange={onChange}/>
-      <button onClick={onClick}>x</button>
+      <Tooltip title="Remove Todolist">
+        <IconButton size="small">
+          <DeleteIcon fontSize="small" onClick={onClick}/>
+        </IconButton>
+      </Tooltip>
     </h3>
   );
 };
