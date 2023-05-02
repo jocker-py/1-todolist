561e337480fc416797597fc573ae5900ba3bdd05
jocker-py <buladzislau@gmail.com>
2023-05-02 12:10:48 +0300

    feat: add props for HeaderTodoListPropsType

--- src/components/HeaderTodoList/HeaderTodoList.tsx
+++ src/components/HeaderTodoList/HeaderTodoList.tsx
@@ -1,13 +1,16 @@
 import React, {FC} from "react";
 import EditSpan from "../EditSpan/EditSpan";
+import {Variant} from "@mui/material/styles/createTypography";
 
 type HeaderTodoListPropsType = {
   title: string
-  onChange: (title: string) => void
+  variant: Variant
+  weight?: "bold" | "normal"
   onClick: () => void
+  onChange: (title: string) => void
 }
 
-const HeaderTodoList: FC<HeaderTodoListPropsType> = ({title, onClick, onChange}) => {
+const HeaderTodoList: FC<HeaderTodoListPropsType> = ({title, variant, weight, onClick, onChange, }) => {
   return (
     <h3>
       <EditSpan title={title} onChange={onChange}/>
