4853b0baed7d6304a38610988652f2e355bfae7a
jocker-py <buladzislau@gmail.com>
2023-05-15 13:52:28 +0300

    feat: add logic for increment age case

--- src/state/user-reducer.ts
+++ src/state/user-reducer.ts
@@ -5,8 +5,11 @@ export const incrementAge = () => ({type: "INCREMENT_AGE"} as const);
 
 export const userReducer = (state: StateType, action: ActionType) => {
   switch (action.type) {
-    case "XXX":
-      return state;
+    case "INCREMENT_AGE":
+      return {
+        ...state,
+        age: state.age + 1,
+      };
     default:
       throw new Error("Unknown action type");
   }
