1d9628551d2e7ac2ea86c37aef22deace0a2ba2c
jocker-py <buladzislau@gmail.com>
2023-05-15 14:22:01 +0300

    feat: add test for test todolist filter case

--- src/state/todolists-reducer.test.ts
+++ src/state/todolists-reducer.test.ts
@@ -1,5 +1,5 @@
 import {v1} from "uuid";
-import {TodoListType} from "../types";
+import {FilterValuesType, TodoListType} from "../types";
 import {addTodolist, changeTodolistTitle, removeTodolist, todolistsReducer} from "./todolists-reducer";
 
 test("should remove first todolist", () => {
@@ -44,4 +44,22 @@ test("should add new todolist", () => {
   expect(endState[0].title).toBe("What to learn");
   expect(endState[1].title).toBe("What to buy");
   expect(endState[2].title).toBe(newTodolistTitle);
+});
+
+test("correct filter of todolist should be changed", () => {
+  let todolistId1 = v1();
+  let todolistId2 = v1();
+
+  let newFilter: FilterValuesType = "completed";
+
+  const startState: Array<TodoListType> = [
+    {id: todolistId1, title: "What to learn", filter: "all"},
+    {id: todolistId2, title: "What to buy", filter: "all"},
+  ];
+
+  const action = changeTodolistFilter(todolistId2, newFilter);
+  const endState = todolistsReducer(startState, action);
+
+  expect(endState[0].filter).toBe("all");
+  expect(endState[1].filter).toBe(newFilter);
 });
