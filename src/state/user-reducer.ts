1107445eb449d05180aab663cfd35962122156f3
jocker-py <buladzislau@gmail.com>
2023-05-15 13:58:13 +0300

    feat: add changeName action creator

--- src/state/user-reducer.ts
+++ src/state/user-reducer.ts
@@ -1,7 +1,10 @@
+import {type} from "@testing-library/user-event/dist/type";
+
 type StateType = { age: number, name: string, childrenCount: number };
 type ActionType = ReturnType<typeof incrementAge> | ReturnType<typeof incrementChildrenCount>;
 
 export const incrementAge = () => ({type: "INCREMENT_AGE"} as const);
+export const changeName = (name: string) => ({type: "CHANGE_NAME", name} as const);
 export const incrementChildrenCount = () => ({type: "INCREMENT_CHILDREN_COUNT"} as const);
 
 export const userReducer = (state: StateType, action: ActionType) => {
