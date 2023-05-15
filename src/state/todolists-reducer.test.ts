736fd2508a50a86a86683f05820652cd1861e7c4
jocker-py <buladzislau@gmail.com>
2023-05-15 14:17:41 +0300

    feat: add import for addTodolist

--- src/state/todolists-reducer.test.ts
+++ src/state/todolists-reducer.test.ts
@@ -1,6 +1,6 @@
 import {v1} from "uuid";
 import {TodoListType} from "../types";
-import {changeTodolistTitle, removeTodolist, todolistsReducer} from "./todolists-reducer";
+import {addTodolist, changeTodolistTitle, removeTodolist, todolistsReducer} from "./todolists-reducer";
 
 test("should remove first todolist", () => {
   const todolistId_1 = v1();
