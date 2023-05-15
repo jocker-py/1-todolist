ba4433d0bff300f3251f4d8e0a97189bb05bd6dc
jocker-py <buladzislau@gmail.com>
2023-05-15 13:49:37 +0300

    feat: add increment age test

---
+++ src/state/user-reducer.test.ts
@@ -0,0 +1,10 @@
+import {userReducer} from "./user-reducer";
+
+test("should increment only age", () => {
+  const startState = {age: 20, childrenCount: 2, name: "Dimych"};
+  const action = incrementAge();
+  const endState = userReducer(startState, action);
+  expect(endState.age).toBe(21);
+  expect(endState.childrenCount).toBe(2);
+  expect(endState.name).toBe("Dimych");
+});
