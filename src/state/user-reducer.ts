35d4666aaa460be9ff0e73299c7a23fb99e05da1
jocker-py <buladzislau@gmail.com>
2023-05-15 13:50:09 +0300

    feat: add incrementAge action creator

--- src/state/user-reducer.ts
+++ src/state/user-reducer.ts
@@ -1,6 +1,8 @@
 type StateType = { age: number, name: string, childrenCount: number };
 type ActionType = any;
 
+export const incrementAge = () => ({type: "INCREMENT_AGE"} as const);
+
 export const userReducer = (state: StateType, action: ActionType) => {
   switch (action.type) {
     case "XXX":
