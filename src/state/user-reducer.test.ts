122fbffbba4868eba91885750aba13e94698455f
jocker-py <buladzislau@gmail.com>
2023-05-15 13:53:15 +0300

    feat: add increment childrenCount test

--- src/state/user-reducer.test.ts
+++ src/state/user-reducer.test.ts
@@ -8,3 +8,12 @@ test("should increment only age", () => {
   expect(endState.childrenCount).toBe(2);
   expect(endState.name).toBe("Dimych");
 });
+
+test("should increment only children count", () => {
+  const startState = {age: 20, childrenCount: 2, name: "Dimych"};
+  const action = incrementChildrenCount();
+  const endState = userReducer(startState, action);
+  expect(endState.age).toBe(20);
+  expect(endState.childrenCount).toBe(3);
+  expect(endState.name).toBe("Dimych");
+});
