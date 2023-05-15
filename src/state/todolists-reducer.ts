60db54cf2109259b0ad67603337a2f9863986e58
jocker-py <buladzislau@gmail.com>
2023-05-15 14:06:01 +0300

    feat: add removeTodolist action creator

--- src/state/todolists-reducer.ts
+++ src/state/todolists-reducer.ts
@@ -2,6 +2,8 @@ import {TodoListType} from "../types";
 
 type ActionType = {type: string};
 
+export const removeTodolist = (id: string) => ({type: "REMOVE_TODOLIST", id});
+
 export const todolistsReducer = (state: Array<TodoListType>, action: ActionType) => {
   switch (action.type) {
     case "XXX":
