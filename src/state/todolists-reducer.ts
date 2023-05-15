6bde71fdd86ab63d1d5a027d0f068fabf5344dd7
jocker-py <buladzislau@gmail.com>
2023-05-15 14:12:20 +0300

    feat: add logic for changeTodolistTitle case

--- src/state/todolists-reducer.ts
+++ src/state/todolists-reducer.ts
@@ -3,12 +3,14 @@ import {TodoListType} from "../types";
 type ActionType = ReturnType<typeof removeTodolist> | ReturnType<typeof changeTodolistTitle>;
 
 export const removeTodolist = (id: string) => ({type: "REMOVE_TODOLIST", id} as const);
-export const changeTodolistTitle = (title: string) => ({type: "CHANGE_TODOLIST_TITLE", title} as const);
+export const changeTodolistTitle = (id: string, title: string) => ({type: "CHANGE_TODOLIST_TITLE", id, title} as const);
 
 export const todolistsReducer = (state: Array<TodoListType>, action: ActionType) => {
   switch (action.type) {
     case "REMOVE_TODOLIST":
       return state.filter(tl => tl.id !== action.id);
+    case "CHANGE_TODOLIST_TITLE":
+      return state.map(tl => tl.id === action.id ? {...tl, title: action.title} : tl);
     default:
       throw new Error("Unknown action type");
   }
