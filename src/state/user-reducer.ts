fb2c33b03c54ce34c0107ec2fcdb2274d13a3f7c
jocker-py <buladzislau@gmail.com>
2023-05-15 13:48:17 +0300

    feat: add userReducer func

--- src/state/user-reducer.ts
+++ src/state/user-reducer.ts
@@ -1,2 +1,11 @@
 type StateType = { age: number, name: string, childrenCount: number };
 type ActionType = any;
+
+export const userReducer = (state: StateType, action: ActionType) => {
+  switch (action.type) {
+    case "XXX":
+      return state;
+    default:
+      throw new Error("Unknown action type");
+  }
+};
