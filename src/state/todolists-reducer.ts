dc7b7bf19a7e28585995312f4c7ee794218e78e7
jocker-py <buladzislau@gmail.com>
2023-05-15 14:09:21 +0300

    feat: add changeTodolistTitle action creator

--- src/state/todolists-reducer.ts
+++ src/state/todolists-reducer.ts
@@ -3,6 +3,7 @@ import {TodoListType} from "../types";
 type ActionType = ReturnType<typeof removeTodolist>;
 
 export const removeTodolist = (id: string) => ({type: "REMOVE_TODOLIST", id} as const);
+export const changeTodolistTitle = (title: string) => ({type: "CHANGE_TODOLIST_TITLE", title} as const);
 
 export const todolistsReducer = (state: Array<TodoListType>, action: ActionType) => {
   switch (action.type) {
