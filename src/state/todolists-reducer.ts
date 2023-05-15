0883b3a241c69e6c8c3bd15c4aeed9ae26b67b6e
jocker-py <buladzislau@gmail.com>
2023-05-15 14:25:11 +0300

    feat: add case for changeTodolistFilter

--- src/state/todolists-reducer.ts
+++ src/state/todolists-reducer.ts
@@ -10,7 +10,11 @@ type ActionType =
 export const addTodolist = (title: string) => ({type: "ADD_TODOLIST", title} as const);
 export const removeTodolist = (id: string) => ({type: "REMOVE_TODOLIST", id} as const);
 export const changeTodolistTitle = (id: string, title: string) => ({type: "CHANGE_TODOLIST_TITLE", id, title} as const);
-export const changeTodolistFilter = (id: string, filter: FilterValuesType) => ({type: "CHANGE_TODOLIST_FILTER", filter} as const);
+export const changeTodolistFilter = (id: string, filter: FilterValuesType) => ({
+  type: "CHANGE_TODOLIST_FILTER",
+  id,
+  filter,
+} as const);
 
 export const todolistsReducer = (state: Array<TodoListType>, action: ActionType) => {
   switch (action.type) {
@@ -20,6 +24,8 @@ export const todolistsReducer = (state: Array<TodoListType>, action: ActionType)
       return state.filter(tl => tl.id !== action.id);
     case "CHANGE_TODOLIST_TITLE":
       return state.map(tl => tl.id === action.id ? {...tl, title: action.title} : tl);
+    case "CHANGE_TODOLIST_FILTER":
+      return state.map(tl => tl.id === action.id ? {...tl, filter: action.filter} : tl);
     default:
       throw new Error("Unknown action type");
   }
