d7e56fe4a1bfc707652c4760f48b80ea432383f2
jocker-py <buladzislau@gmail.com>
2023-05-15 14:08:35 +0300

    feat: add test change name of Todolist

--- src/state/todolists-reducer.test.ts
+++ src/state/todolists-reducer.test.ts
@@ -14,3 +14,18 @@ test("should remove first todolist", () => {
   expect(endState.length).toBe(1);
   expect(endState[0].id).toBe(todolistId_2);
 });
+
+test("should change name of second todolist", () => {
+  const todolistId_1 = v1();
+  const todolistId_2 = v1();
+  const startState: Array<TodoListType> = [
+    {id: todolistId_1, title: "What to learn", filter: "all"},
+    {id: todolistId_2, title: "What to buy", filter: "all"},
+  ];
+  const newTodolistTitle = "New todolist";
+  const action = changeTodolistTitle(todolistId_2, newTodolistTitle);
+  const endState = todolistsReducer(startState, action);
+
+  expect(endState[0].title).toBe("What to learn");
+  expect(endState[1].title).toBe(newTodolistTitle);
+});
