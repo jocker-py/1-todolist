571bab2a376f8006f18dfa97a97e3fd6dbd1cfdd
jocker-py <buladzislau@gmail.com>
2023-06-04 13:00:40 +0300

    fix: fix test bug

--- src/state/todolists-reducer.test.ts
+++ src/state/todolists-reducer.test.ts
@@ -24,13 +24,14 @@ beforeEach(() => {
 test("should remove first todolist", () => {
   const action = removeTodolist(todolistId_1);
   const endState = todolistsReducer(startState, action);
+
   expect(endState.length).toBe(1);
   expect(endState[0].id).toBe(todolistId_2);
 });
 
 test("should change name of second todolist", () => {
   const newTodolistTitle = "New todolist";
-  const action = changeTodolistTitle(todolistId_2, newTodolistTitle);
+  const action = changeTodolistTitle(newTodolistTitle, todolistId_2);
   const endState = todolistsReducer(startState, action);
 
   expect(endState[0].title).toBe("What to learn");
@@ -49,7 +50,7 @@ test("should add new todolist", () => {
 
 test("correct filter of todolist should be changed", () => {
   let newFilter: FilterValuesType = "completed";
-  const action = changeTodolistFilter(todolistId_2, newFilter);
+  const action = changeTodolistFilter(newFilter, todolistId_2);
   const endState = todolistsReducer(startState, action);
 
   expect(endState[0].filter).toBe("all");
