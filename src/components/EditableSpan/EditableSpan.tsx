b180ddef425c6183fe855d1900c03278e869d44e
jocker-py <buladzislau@gmail.com>
2023-07-10 22:13:44 +0300

    feat: add EditableSpan component

---
+++ src/components/EditableSpan/EditableSpan.tsx
@@ -0,0 +1,47 @@
+import React, { FC, useState } from "react";
+
+type EditableSpanPropsType = {
+  title: string;
+  changeTitle: (title: string) => void;
+};
+
+export const EditableSpan: FC<EditableSpanPropsType> = ({
+  title,
+  changeTitle,
+}) => {
+  const [text, setText] = useState(title);
+  const [editMode, setEditMode] = useState(false);
+  const [error, setError] = useState<null | string>(null);
+
+  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (error) setError(null);
+    setText(e.target.value);
+  };
+
+  const onBlurHandler = () => {
+    if (text.trim().length) {
+      changeTitle(text);
+      editModeOff();
+    } else {
+      setError("The field is required");
+    }
+  };
+
+  const editModeOn = () => setEditMode(true);
+  const editModeOff = () => setEditMode(false);
+
+  return (
+    <div>
+      {editMode ? (
+        <input
+          value={text}
+          onChange={onChangeHandler}
+          onBlur={onBlurHandler}
+          autoFocus
+        />
+      ) : (
+        <span onDoubleClick={editModeOn}>{title}</span>
+      )}
+    </div>
+  );
+};
