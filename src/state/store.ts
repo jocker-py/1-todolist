10d57a9b8b011638aedf158bce1e0fcaf81691d3
jocker-py <buladzislau@gmail.com>
2023-07-11 22:06:59 +0300

    feat: add type AppThunk

--- src/state/store.ts
+++ src/state/store.ts
@@ -3,10 +3,13 @@ import {
   combineReducers,
   legacy_createStore as createStore,
 } from "redux";
-import { todolistsReducer } from "../features/todolistsReducer";
+import {
+  TodolistActionsType,
+  todolistsReducer,
+} from "../features/todolistsReducer";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
-import thunk from "redux-thunk";
-import { tasksReducer } from "../features/tasksReducer";
+import thunk, { ThunkAction, ThunkDispatch } from "redux-thunk";
+import { TasksActionsType, tasksReducer } from "../features/tasksReducer";
 
 // root
 const rootReducer = combineReducers({
@@ -17,11 +20,19 @@ const rootReducer = combineReducers({
 // store
 export const store = createStore(rootReducer, applyMiddleware(thunk));
 
-// typed hooks
-export const useAppDispatch: DispatchFunc = useDispatch;
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
-
 //types
-type RootState = ReturnType<typeof rootReducer>;
-export type AppDispatch = typeof store.dispatch;
-type DispatchFunc = () => AppDispatch;
+type AppActionsType = TodolistActionsType | TasksActionsType;
+export type AppState = ReturnType<typeof store.getState>;
+export type AppDispatch = ThunkDispatch<AppState, unknown, AppActionsType>;
+
+// typed hooks
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<AppState> = useSelector;
+//: DispatchFunc = useDispatch;
+
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  AppState,
+  unknown,
+  AppActionsType
+>;
