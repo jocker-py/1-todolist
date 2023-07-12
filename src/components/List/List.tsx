3b0de22e1e823bcae513e4bb97c490f0c10858f7
jocker-py <buladzislau@gmail.com>
2023-07-12 21:55:29 +0300

    feat: add List component

---
+++ src/components/List/List.tsx
@@ -0,0 +1,15 @@
+import React, { CSSProperties } from "react";
+
+type ListPropsType<T> = {
+  items: Array<T>;
+  renderItem: (item: T) => React.ReactNode;
+  style?: CSSProperties;
+};
+
+export const List = <T extends {}>({
+  items,
+  renderItem,
+  style,
+}: ListPropsType<T>) => {
+  return <div style={style}>{items && items.map(renderItem)}</div>;
+};
