6cfc5845030b14ada02d0b7ce61e8fe8687e7030
jocker-py <buladzislau@gmail.com>
2023-07-12 21:57:19 +0300

    feat: add addTask, removeTask, updateTask behavior

--- src/features/tasksReducer.tsx
+++ src/features/tasksReducer.tsx
@@ -1,14 +1,50 @@
 // initial
 import { EntityStatusType } from "./todolistsReducer";
 import { todolistsAPI } from "../api/api";
-import { AppDispatch } from "../state/store";
+import { AppThunk } from "../state/store";
+import { setAppError, setAppStatus } from "../app/appReducer";
 
 const initialState: TasksStateType = {};
 
 // reducer
-export const tasksReducer = (state = initialState, action: ActionsType) => {
+export const tasksReducer = (
+  state = initialState,
+  action: TasksActionsType
+) => {
   switch (action.type) {
     case "TASKS/SET_TASKS":
+      return {
+        ...state,
+        [action.todolistID]: action.tasks.map((task) => ({
+          ...task,
+          entityStatus: "idle",
+        })),
+      };
+
+    case "TASKS/ADD_TASK":
+      return {
+        ...state,
+        [action.todolistID]: [
+          { ...action.task, entityStatus: "idle" },
+          ...state[action.todolistID],
+        ],
+      };
+
+    case "TASKS/UPDATE_TASK":
+      return {
+        ...state,
+        [action.todolistID]: state[action.todolistID].map((task) =>
+          task.id === action.taskID ? { ...task, ...action.task } : task
+        ),
+      };
+
+    case "TASKS/REMOVE_TASK":
+      return {
+        ...state,
+        [action.todolistID]: state[action.todolistID].filter(
+          (task) => task.id !== action.taskID
+        ),
+      };
     default:
       return state;
   }
@@ -18,39 +54,107 @@ export const tasksReducer = (state = initialState, action: ActionsType) => {
 export const setTasks = (todolistID: string, tasks: Array<TaskType>) =>
   ({ type: "TASKS/SET_TASKS", todolistID, tasks } as const);
 
+export const addTask = (todolistID: string, task: TaskType) =>
+  ({ type: "TASKS/ADD_TASK", todolistID, task } as const);
+
+export const updateTask = (
+  taskID: string,
+  todolistID: string,
+  task: TaskModelType
+) => ({ type: "TASKS/UPDATE_TASK", taskID, todolistID, task } as const);
+
+export const removeTask = (taskID: string, todolistID: string) =>
+  ({ type: "TASKS/REMOVE_TASK", taskID, todolistID } as const);
+
 // thunks
-export const fetchTasks = (todolistID: string) => (dispatch: AppDispatch) => {
-  todolistsAPI.getTasks(todolistID).then((data) => {
-    if (data) {
-      dispatch(setTasks(todolistID, data.items));
-    }
-  });
-};
+export const fetchTasks =
+  (todolistID: string): AppThunk =>
+  (dispatch) => {
+    dispatch(setAppStatus("loading"));
+    todolistsAPI.getTasks(todolistID).then((data) => {
+      if (data) {
+        dispatch(setTasks(todolistID, data.items));
+        dispatch(setAppStatus("succeeded"));
+      }
+    });
+  };
+
+export const createTask =
+  (todolistID: string, title: string): AppThunk =>
+  (dispatch) => {
+    dispatch(setAppStatus("loading"));
+    todolistsAPI.createTask(todolistID, title).then((res) => {
+      if (res && res.resultCode === 0) {
+        dispatch(addTask(todolistID, res.data.item));
+        dispatch(setAppStatus("succeeded"));
+      } else {
+        dispatch(setAppStatus("failed"));
+        dispatch(setAppError("Some error"));
+      }
+    });
+  };
+export const updateTaskModel =
+  (taskID: string, todolistID: string, task: TaskModelType): AppThunk =>
+  (dispatch) => {
+    dispatch(setAppStatus("loading"));
+    todolistsAPI.updateTaskModel(taskID, todolistID, task).then((res) => {
+      if (res && res.resultCode === 0) {
+        dispatch(updateTask(taskID, todolistID, task));
+        dispatch(setAppStatus("succeeded"));
+      } else {
+        dispatch(setAppStatus("failed"));
+        dispatch(setAppError("Some error"));
+      }
+    });
+  };
+export const deleteTask =
+  (taskID: string, todolistID: string): AppThunk =>
+  (dispatch) => {
+    dispatch(setAppStatus("loading"));
+    todolistsAPI.deleteTask(taskID, todolistID).then((res) => {
+      if (res && res.resultCode === 0) {
+        dispatch(removeTask(taskID, todolistID));
+        dispatch(setAppStatus("succeeded"));
+      } else {
+        dispatch(setAppStatus("failed"));
+        dispatch(setAppError("Some error"));
+      }
+    });
+  };
 
 // types
-//
-// enum Status {
-//   New = 0,
-// }
 
-export type TaskType = {
-  description: string | null;
+export enum Status {
+  New = 0,
+  Completed = 1,
+  Active = 2,
+}
+
+export type TaskModelType = {
   title: string;
+  description: string | null;
   completed: boolean;
-  status: number;
+  status: Status;
   priority: number;
   startDate: string | null;
   deadline: string | null;
+};
+
+export type TaskType = TaskModelType & {
   id: string;
   todoListId: string;
   order: number;
   addedDate: string;
 };
 
-type TaskDomainType = TaskType & {
+export type TaskDomainType = TaskType & {
   entityStatus: EntityStatusType;
 };
 
 type TasksStateType = { [key: string]: Array<TaskDomainType> };
 
-type ActionsType = ReturnType<typeof setTasks>;
+export type TasksActionsType =
+  | ReturnType<typeof setTasks>
+  | ReturnType<typeof addTask>
+  | ReturnType<typeof removeTask>
+  | ReturnType<typeof updateTask>;
