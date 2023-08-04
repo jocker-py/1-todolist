f20ffed94d1e92e159f067167f1c7114d558b63f
jocker-py <buladzislau@gmail.com>
2023-08-04 17:40:37 +0300

    feat: add useTodolistList

---
+++ src/features/TodolistList/useTodolistList.ts
@@ -0,0 +1,35 @@
+import { useCallback, useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../../state/store";
+import {
+  createTodolist,
+  fetchTodolists,
+  TodolistsStateType,
+} from "./Todolist/todolistsReducer";
+
+export const useTodolistList = () => {
+  const dispatch = useAppDispatch();
+  const todolists = useAppSelector(
+    (state) => state.todolists
+  ) as TodolistsStateType;
+
+  const { entityStatus } = useAppSelector((state) => state.app);
+  const { isLoggedIn } = useAppSelector((state) => state.auth);
+
+  const isLoading = entityStatus === "loading";
+
+  const addTodolist = useCallback(
+    (title: string) => dispatch(createTodolist(title)),
+    []
+  );
+
+  useEffect(() => {
+    if (isLoggedIn) dispatch(fetchTodolists());
+  }, []);
+
+  return {
+    addTodolist,
+    todolists,
+    isLoading,
+    isLoggedIn,
+  };
+};
