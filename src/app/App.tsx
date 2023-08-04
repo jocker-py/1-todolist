0f4bfb5b32b51ebc4e0146cf476bc2c79acc4082
jocker-py <buladzislau@gmail.com>
2023-08-04 17:28:39 +0300

    feat: add effect initializeApp

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "../components/Header/Header";
 import TodolistList from "../features/TodolistList/TodolistList";
 import { Login } from "../features/Login/Login";
 import { Navigate, Routes, Route } from "react-router-dom";
-import { useAppSelector } from "../state/store";
+import { useAppDispatch, useAppSelector } from "../state/store";
+import { initializeApp } from "./appReducer";
 import Preloader from "../components/Preloader/Preloader";
 
 function App() {
+  const dispatch = useAppDispatch();
   const { isInitialized } = useAppSelector((state) => state.app);
+
+  useEffect(() => {
+    dispatch(initializeApp());
+  }, []);
 
   if (!isInitialized) {
     return <Preloader />;
