03e7639d29471a8b1f546f8c9e39a82cae9181df
jocker-py <buladzislau@gmail.com>
2023-08-04 17:17:48 +0300

    refactor: remove catch part of Promises

--- src/api/api.ts
+++ src/api/api.ts
@@ -15,66 +15,43 @@ export const todolistsAPI = {
   getTodolists: () =>
     instance
       .get("todo-lists")
-      .then<Array<TodolistType>>((res) => res.data)
-      .catch((err) =>
-        console.error("TodolistAPI error get Todolists : " + err.message)
-      ),
+      .then<Array<TodolistType>>((res) => res.data),
 
   updateTodolistTitle: (todolistID: string, title: string) =>
     instance
       .put(`todo-lists/${todolistID}`, { title })
-      .then<ResponseType>((res) => res.data)
-      .catch((err) =>
-        console.error("TodolistAPI error put Todolist : " + err.message)
-      ),
+      .then<ResponseType>((res) => res.data),
+
 
   createTodolist: (title: string) =>
     instance
       .post("todo-lists", { title })
-      .then<ResponseType<{ item: TodolistType }>>((res) => res.data)
-      .catch((err) =>
-        console.error("TodolistAPI error post Todolist: " + err.message)
-      ),
+      .then<ResponseType<{ item: TodolistType }>>((res) => res.data),
 
   deleteTodolist: (todolistID: string) =>
     instance
       .delete(`todo-lists/${todolistID}`)
-      .then<ResponseType>((res) => res.data)
-      .catch((err) =>
-        console.error("TodolistAPI error delete Todolist: " + err.message)
-      ),
+      .then<ResponseType>((res) => res.data),
 
   getTasks: (todolistID: string) =>
     instance
       .get(`todo-lists/${todolistID}/tasks`)
-      .then<{ items: Array<TaskType> }>((res) => res.data)
-      .catch((err) =>
-        console.error("TodolistAPI error get Tasks : " + err.message)
-      ),
+      .then<{ items: Array<TaskType> }>((res) => res.data),
 
   createTask: (todolistID: string, title: string) =>
     instance
       .post(`todo-lists/${todolistID}/tasks`, { title })
-      .then<ResponseType<{ item: TaskType }>>((res) => res.data)
-      .catch((err) =>
-        console.error("TodolistAPI error create Task : " + err.message)
-      ),
+      .then<ResponseType<{ item: TaskType }>>((res) => res.data),
 
   updateTaskModel: (taskID: string, todolistID: string, task: TaskModelType) =>
     instance
       .put(`todo-lists/${todolistID}/tasks/${taskID}`, { ...task })
-      .then<ResponseType>((res) => res.data)
-      .catch((err) =>
-        console.error("TodolistAPI error update TaskModel : " + err.message)
-      ),
+      .then<ResponseType>((res) => res.data),
 
   deleteTask: (taskID: string, todolistID: string) =>
     instance
       .delete(`todo-lists/${todolistID}/tasks/${taskID}`)
-      .then<ResponseType>((res) => res.data)
-      .catch((err) =>
-        console.error("TodolistAPI error delete Task : " + err.message)
-      ),
+      .then<ResponseType>((res) => res.data),
 };
 
 // type
