4ebfa773e51a65e4ba9a67db2b23c998f3cf60a5
jocker-py <buladzislau@gmail.com>
2023-08-04 17:36:16 +0300

    feat: add Login

---
+++ src/features/Login/Login.tsx
@@ -0,0 +1,113 @@
+import React from "react";
+import Grid from "@mui/material/Grid";
+import Checkbox from "@mui/material/Checkbox";
+import FormControl from "@mui/material/FormControl";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormGroup from "@mui/material/FormGroup";
+import FormLabel from "@mui/material/FormLabel";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import { useFormik } from "formik";
+import { useAppDispatch, useAppSelector } from "../../state/store";
+import { login } from "./auth-reducer";
+import { Navigate } from "react-router-dom";
+
+type FormikErrorType = {
+  email?: string;
+  password?: string;
+  rememberMe?: boolean;
+};
+
+export const Login = () => {
+  const dispatch = useAppDispatch();
+  const isLoggedIn = useAppSelector((state) => state.auth.isLoggedIn);
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+      rememberMe: false,
+    },
+    validate: (values) => {
+      const errors: FormikErrorType = {};
+      if (!values.email) {
+        errors.email = "Required";
+      } else if (
+        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+      ) {
+        errors.email = "Invalid email address";
+      }
+
+      if (!values.password) {
+        errors.password = "Required";
+      } else if (values.password.length < 3) {
+        errors.password = "Password should be more than 3 symbols";
+      }
+
+      return errors;
+    },
+    onSubmit: (values) => {
+      dispatch(login(values));
+      formik.resetForm();
+    },
+  });
+
+  if (isLoggedIn) {
+    return <Navigate to={"/"} />;
+  }
+
+  return (
+    <Grid container justifyContent={"center"}>
+      <Grid item justifyContent={"center"}>
+        <form onSubmit={formik.handleSubmit}>
+          <FormControl>
+            <FormLabel>
+              <p>
+                To log in get registered
+                <a
+                  href={"https://social-network.samuraijs.com/"}
+                  target={"_blank"}
+                  rel="noreferrer"
+                >
+                  {" "}
+                  here
+                </a>
+              </p>
+              <p>or use common test account credentials:</p>
+              <p>Email: free@samuraijs.com</p>
+              <p>Password: free</p>
+            </FormLabel>
+            <FormGroup>
+              <TextField
+                label="Email"
+                margin="normal"
+                error={!!(formik.touched.email && formik.errors.email)}
+                helperText={(formik.touched.email && formik.errors.email) || ""}
+                {...formik.getFieldProps("email")}
+                autoComplete={"email"}
+              />
+              <TextField
+                type="password"
+                label="Password"
+                margin="normal"
+                error={!!(formik.touched.password && formik.errors.password)}
+                helperText={
+                  (formik.touched.password && formik.errors.password) || ""
+                }
+                {...formik.getFieldProps("password")}
+                autoComplete={"current-password"}
+              />
+              <FormControlLabel
+                label={"Remember me"}
+                control={<Checkbox />}
+                {...formik.getFieldProps("rememberMe")}
+              />
+              <Button type={"submit"} variant={"contained"} color={"primary"}>
+                Login
+              </Button>
+            </FormGroup>
+          </FormControl>
+        </form>
+      </Grid>
+    </Grid>
+  );
+};
