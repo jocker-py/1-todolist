82f2aebabb0c5c9af31e74d3d7b37a21031163eb
jocker-py <buladzislau@gmail.com>
2023-08-04 17:54:40 +0300

    feat: add handleNetworkAppError & handleServerAppError

---
+++ src/utils/error/error.ts
@@ -0,0 +1,31 @@
+import {
+  setAppError,
+  SetAppErrorType,
+  setAppStatus,
+  SetAppStatusType,
+} from "../../app/appReducer";
+import { AxiosError } from "axios";
+import { Dispatch } from "redux";
+import { ResponseType } from "../../api/api";
+
+export const handleServerAppError = (
+  e: AxiosError,
+  dispatch: DispatchErrorType
+) => {
+  dispatch(setAppError(e.message));
+  dispatch(setAppStatus("failed"));
+};
+
+export const handleNetworkAppError = (
+  res: ResponseType,
+  dispatch: DispatchErrorType
+) => {
+  if (res.messages.length) {
+    dispatch(setAppError(res.messages[0]));
+  } else {
+    dispatch(setAppError("Some unknown error"));
+  }
+  dispatch(setAppStatus("failed"));
+};
+
+type DispatchErrorType = Dispatch<SetAppStatusType | SetAppErrorType>;
