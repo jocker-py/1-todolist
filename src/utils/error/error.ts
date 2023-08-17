a76fa49a1b081048f8b956f2efa5cccd1d2abba6
jocker-py <buladzislau@gmail.com>
2023-08-18 00:04:45 +0300

    refactor: use appActions

--- src/utils/error/error.ts
+++ src/utils/error/error.ts
@@ -1,31 +1,21 @@
-import {
-  setAppError,
-  SetAppErrorType,
-  setAppStatus,
-  SetAppStatusType,
-} from "../../app/appReducer"
 import { AxiosError } from "axios"
+import { ResponseType } from "api/api"
+import { appActions } from "app/appReducer"
 import { Dispatch } from "redux"
-import { ResponseType } from "../../api/api"
 
-export const handleServerAppError = (
-  e: AxiosError,
-  dispatch: DispatchErrorType,
-) => {
-  dispatch(setAppError(e.message))
-  dispatch(setAppStatus("failed"))
+export const handleServerAppError = (e: AxiosError, dispatch: Dispatch) => {
+  dispatch(appActions.setAppError({ error: e.message }))
+  dispatch(appActions.setAppStatus({ entityStatus: "failed" }))
 }
 
 export const handleNetworkAppError = (
   res: ResponseType,
-  dispatch: DispatchErrorType,
+  dispatch: Dispatch,
 ) => {
   if (res.messages.length) {
-    dispatch(setAppError(res.messages[0]))
+    dispatch(appActions.setAppError({ error: res.messages[0] }))
   } else {
-    dispatch(setAppError("Some unknown error"))
+    dispatch(appActions.setAppError({ error: "Some unknown error" }))
   }
-  dispatch(setAppStatus("failed"))
+  dispatch(appActions.setAppStatus({ entityStatus: "failed" }))
 }
-
-type DispatchErrorType = Dispatch<SetAppStatusType | SetAppErrorType>
