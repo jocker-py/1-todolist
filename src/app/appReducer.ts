79ef0b1c5ed4ded646b16fe81c71c5054d5f9bd8
jocker-py <buladzislau@gmail.com>
2023-08-18 00:06:27 +0300

    refactor: use redux/toolkit

--- src/app/appReducer.ts
+++ src/app/appReducer.ts
@@ -1,82 +1,41 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { EntityStatusType } from "features/TodolistList/Todolist/todolistsReducer";
+
 // initial state
-import { EntityStatusType } from "../features/TodolistList/Todolist/todolistsReducer"
-import { authAPI, ResultCode } from "../api/api"
-import { Dispatch } from "redux"
-import {
-  handleNetworkAppError,
-  handleServerAppError,
-} from "../utils/error/error"
-import { AxiosError } from "axios"
-import { AuthActionsType, setIsLoggedIn } from "../features/Login/auth-reducer"
+const initialState = {
+  entityStatus: "idle" as EntityStatusType,
+  error: null as null | string,
+  isInitialized: false
+};
 
-const initialState: AppStateType = {
-  entityStatus: "idle",
-  error: null,
-  isInitialized: false,
-}
+export type InitialAppStateType = typeof initialState
 
-// reducer
-export const appReducer = (state = initialState, action: AppActionsType) => {
-  switch (action.type) {
-    case "APP/SET_STATUS":
-      return { ...state, entityStatus: action.entityStatus }
-    case "APP/SET_ERROR":
-      return { ...state, error: action.error }
-    case "APP/SET_INITIALIZED":
-      return { ...state, isInitialized: true }
-    default:
-      return state
+//slice
+const slice = createSlice({
+  name: "App",
+  initialState,
+  reducers: {
+    setAppError: (state, action: PayloadAction<{ error: null | string }>) => {
+      state.error = action.payload.error;
+    },
+    setAppStatus: (
+      state,
+      action: PayloadAction<{ entityStatus: EntityStatusType }>
+    ) => {
+      state.entityStatus = action.payload.entityStatus;
+    },
+    setAppInitialized: (
+      state,
+      action: PayloadAction<{ isInitialized: boolean }>
+    ) => {
+      state.isInitialized = action.payload.isInitialized;
+    }
   }
-}
+});
+
 
 // actions
-export const setAppStatus = (entityStatus: EntityStatusType) =>
-  ({
-    type: "APP/SET_STATUS",
-    entityStatus,
-  }) as const
+export const appActions = slice.actions;
 
-export const setAppError = (error: null | string) =>
-  ({
-    type: "APP/SET_ERROR",
-    error,
-  }) as const
-
-export const setAppInitialized = () =>
-  ({
-    type: "APP/SET_INITIALIZED",
-  }) as const
-
-export const initializeApp =
-  () => (dispatch: Dispatch<AppActionsType | AuthActionsType>) => {
-    authAPI
-      .me()
-      .then((res) => {
-        if (res.resultCode === ResultCode.OK) {
-          dispatch(setIsLoggedIn(true))
-        } else {
-          handleNetworkAppError(res, dispatch)
-        }
-      })
-      .catch((e: AxiosError) => {
-        handleServerAppError(e, dispatch)
-      })
-      .finally(() => {
-        dispatch(setAppInitialized())
-      })
-  }
-
-// type
-type AppStateType = {
-  entityStatus: EntityStatusType
-  error: null | string
-  isInitialized: boolean
-}
-
-export type SetAppStatusType = ReturnType<typeof setAppStatus>
-export type SetAppErrorType = ReturnType<typeof setAppError>
-
-export type AppActionsType =
-  | SetAppStatusType
-  | SetAppErrorType
-  | ReturnType<typeof setAppInitialized>
+// reducer
+export const appReducer = slice.reducer;
